508cc9a07eab0c902e7a02e118b9aff9531b3ccc
Wambita Fana <wambitafana@gmail.com>
2019-04-17 12:30:44 +0300

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_movies,get_movie,search_movie
+from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
 from ..models import Review
 
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=d8685c540bd902977b795dacc2a55a20
+export SECRET_KEY=4598e6dba4bec58c1b59552c6409272aea31978ab8159f11a1
+
+python3.6 manage.py server
